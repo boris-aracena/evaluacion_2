5ae62b595aa7126680d5f892d29c4e81b381e52d
boris <aracena.boris@gmail.com>
2020-11-13 11:34:14 -0300

    subido archivo con seudocodigo en el interior

---
+++ abaco.py
@@ -0,0 +1,13 @@
+'''
+imprimir instrucciones al usuario
+usuario ingresa cifra
+    separar cifra en unidad-decena-centena, etc {'1':5, '10': 2, '100':4, '1.000':4, '10.000':6, '100.000':6}
+    aplicarle el formato
+    Almacenamiento en lista['657.340', '1.051', '89']
+    imprimir abaco
+usuario ingresa nueva cifra
+    repetir paso 3-4-5-6
+si usuario ingresa salir
+    imprimir lista de numeros
+    byebye
+'''
